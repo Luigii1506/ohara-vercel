c81cd5f9e04861edc67ceed26931e95389286a98
Luis Gerardo <luis.encinas1506@gmail.com>
2026-01-22 16:56:25 -0800

    Update route.ts

--- app/api/lists/route.ts
+++ app/api/lists/route.ts
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest) {
           select: { cards: true },
         },
         cards: {
-          include: {
+          select: {
             card: {
               select: {
                 id: true,
